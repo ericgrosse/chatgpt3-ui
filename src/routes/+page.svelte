e492d3be7496d7293b0f344028a4743692a0add4
Eric Grosse <ericgrosse1@gmail.com>
2023-01-27 14:34:55 -0500

    Implemented API call and basic UI layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,105 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import axios from 'axios';
+  import { API_KEY } from '../components/ApiKey.js';
+
+  let chatGPT3Response = '';
+
+  async function handleClick() {
+    try {
+      const response = await axios.post(
+        'https://api.openai.com/v1/completions',
+        {
+          model: 'text-davinci-003', // string, required
+          prompt: '', // string or array, optional, defaults to <|endoftext|>
+          suffix: null, // string, optional, defaults to null
+          max_tokens: 150, // integer, optional, defaults to 16
+          temperature: 1, // number, optional, defaults to 1
+          top_p: 1, // number, optional, defaults to 1
+          n: 1, // integer, optional, defaults to 1
+          stream: false, // boolean, optional, defaults to false
+          logprobs: null, // integer, optional, defaults to null
+          echo: false, // boolean, optional, defaults to false
+          stop: null, // string or array, optional, defaults to null
+          presence_penalty: 0, // number, optional, defaults to 0
+          frequency_penalty: 0, // number, optional, defaults to 0
+          best_of: 1, // integer, optional, defaults to 1
+          //logit_bias: null, // map, optional, defaults to null
+          //user: null, // string, optional
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${API_KEY}`,
+          },
+        });
+      
+      console.log(response.data.choices[0].text);
+      chatGPT3Response = response.data.choices[0].text; 
+
+  } catch (error) {
+      console.log('Inside error handler');
+      console.log(error);
+    }
+  }
+  </script>
+  
+  <main class="container">
+    <h3>Required Fields</h3>
+    <input type="text" placeholder="prompt" />
+    <h3>Optional Fields</h3>
+
+    <div class="optional-fields-container">
+      <div class="optional-fields">
+        <input type="text" placeholder="suffix" />
+        <input type="text" placeholder="max_tokens" />
+        <input type="text" placeholder="temperature" />
+      </div>
+      <div class="optional-fields">
+        <input type="text" placeholder="top_n" />
+        <input type="text" placeholder="n" />
+        <input type="text" placeholder="stream" />
+      </div>
+      <div class="optional-fields">
+        <input type="text" placeholder="logprobs" />
+        <input type="text" placeholder="echo" />
+        <input type="text" placeholder="stop" />
+      </div>
+      <div class="optional-fields">
+        <input type="text" placeholder="presence_penalty" />
+        <input type="text" placeholder="frequency_penalty" />
+        <input type="text" placeholder="best_of" />
+      </div>
+    </div>
+
+    <button class="generate-short-story" on:click={handleClick}>Generate Text</button>
+    <p>Output</p>
+    <p>{chatGPT3Response}</p>
+  </main>
+  
+  <style>
+    .generate-short-story {
+      background-color: #2196f3;
+      color: white;
+      padding: 12px 24px;
+      border-radius: 4px;
+      font-size: 16px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      transition-duration: 0.4s;
+      cursor: pointer;
+      box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+                  0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+    }
+
+    .generate-short-story:hover {
+      background-color: #1976d2;
+    }
+
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+  </style>
